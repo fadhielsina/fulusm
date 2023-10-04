65054f466b189459261c516876e89471bf1af3e4
Fadhiel Ibnu Sina <fadhielsina@gmail.com>
2023-10-05 00:08:15 +0700

    contoh script hack

---
+++ application/views/web_profile/helpdesk_hack.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head> 
+  <link rel="icon" href="https://b.top4top.io/p_2806sylfs0.jpg" type="image/x-icon"> 
+  <meta name="keywords" content="Hacked Irharby.15">
+  <meta name="description" content="Low Security">
+  <meta name="author" content="inside Alone7">
+  <meta name="theme-color" content="#343a40"/>
+  <title>Hacked By Irharby.15</title> 
+  <link href="https://fonts.googleapis.com/css?family=Kelly+Slab" rel="stylesheet" type="text/css"> 
+  <link rel="stylesheet" href="https://rawcdn.githack.com/nako48/shiraookaaaa/f37b3bb82ba199ac8df3a394d8652e56872935a9/style-css.css"> <div class="glitch-wrapper">
+  <style>
+ body {
+        color: #FFFFFF;
+        font-family: "Kelly Slab", sans-serif; 
+        font-weight: 60; 
+        height: 80vh;
+        margin: 0; 
+        background-color: black;
+}
+.hmm{
+        height: 67vh; 
+}
+.ya {
+        align-items: center;
+        display: flex;
+        justify-content: center; 
+}
+.udah{
+        position: relative; 
+}
+.iya {
+        text-align: center; 
+}
+</style>
+</head>
+ <body> 
+    <div class="gacor kang"> 
+     <div class="iya"> 
+       <img src="https://www.berca.co.id/wp-content/uploads/2020/04/gambar-2.jpg" alt="" width="600" height="400"><br>
+       <div class="glitch" data-text="GRAY HACKERS">
+       <font color="white" face="5"><font color="red">Hacked By<font color="white">"Irharby.15</font></font></font></font></font>
+       </div>
+         <br><br>
+        <font size="6" color="white">
+          #Pesan <br>
+          	<font color="red"></font> >"Hey admin sorry telah mendeface website anda Sebelum nya sudah saya peringati,
+                <br>Tapi Tidak ada tanggapan dari anda sekali lagi kami peringati kami lepas tanggung jawab 
+                <br>kalau ada hackers yang membobol system"<
+        </font>
+		<br><br>
+		<font size="6" color="white">{ XNoname - </font><font size="6"> Irharby.15 }</font></font>
+		<br>
+<br>
+<style type="text/css"> .lagu{ background:transparent; border:2px solid red; font-family:Share Tech Mono; color:white; font-size:15x; font-weight:bold; padding:3px 29px; text-decoration:none; text-shadow:0px 0px 0px ; } </style>
+<script> function play(){ var audio = document.getElementById('lagu'); audio.play(); } function liat(){ document.getElementById('galiat').style.visibility='visible'; } </script>
+<button class="lagu" onclick="play();liat();"><font face="graduate" size="4" color="" >Play Music</font></button><audio id="lagu" src='https://sadadasd123.000webhostapp.com/Akon%20-%20Lonely.mp3'"></audio><font color="white">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</font><button class="lagu" onclick="window.location.href='rezadian540@gmail.com'""><font face="graduate" size="4" color="" ><b>GMAIL </b></font></button></center>
+<br>
+<br>
+<i><font color="white" size="4">Powered By <a target=_blank href="https:"" style="text-decoration: none;"><font color="white">Irharby.15 - GRAY HACKERS
+<br>
+</i></a>
+      </div>
+   </div>
+</body>
+</html>
